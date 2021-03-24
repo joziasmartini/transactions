08dea9826c96497360b5283db3ea3eadcdb0276b
Diego Becker <diegoluizbecker@gmail.com>
2021-03-24 13:03:29 -0300

    Update transaction1.py

--- transaction1.py
+++ transaction1.py
@@ -10,6 +10,7 @@ class Banco:
             self.connection = psycopg2.connect(user="postgres", password="root", host="127.0.0.1", database="transactions")
             self.connection.autocommit = False
             self.cursor = self.connection.cursor()
+            self.df = self.pd.read_csv('data.csv')
             
         except (Exception, psycopg2.DatabaseError) as error:
             print("Deu caca\n")
@@ -29,6 +30,14 @@ class Banco:
         print('hello')
 
     async def executeImplicit(self):
+        try:
+            for x in range(10002): # (10002):
+                print(df)
+        
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Deu caca\n")
+            print(error)
+            connection.rollback()
         print('hello 1')
 
     async def showRows(self):
