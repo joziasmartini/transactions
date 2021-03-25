ee1f76fa227a21973bf7dcd6d4fccc205df7ba9e
Diego Becker <diegoluizbecker@gmail.com>
2021-03-24 21:24:49 -0300

    Update transaction1.py

--- transaction1.py
+++ transaction1.py
@@ -20,13 +20,33 @@ class Banco:
         await asyncio.gather(self.executeExplicit())
         fim = timeit.default_timer()
         print ('duracao explicita = : %f' % (fim - inicio))
-        inicio = timeit.default_timer()
-        await asyncio.gather(self.executeImplicit())
-        fim = timeit.default_timer()
-        print ('duracao implicita = : %f' % (fim - inicio))
+        #inicio = timeit.default_timer()
+        #await asyncio.gather(self.executeImplicit())
+        #fim = timeit.default_timer()
+        #print ('duracao implicita = : %f' % (fim - inicio))
 
     async def executeExplicit(self):
-        print('hello')
+        try:
+            query = "BEGIN"
+            self.cursor.execute(query)
+
+            self.df = pd.read_csv('data.csv')
+            for x in range(10002): # (10002):
+                #print(self.df['Product Name'] [x])
+                ab = x + 10
+                query = "insert into product values( " + str(ab) + ", " + self.df['Product Name'] [x] + ")"
+                # print(query)
+                self.cursor.execute(query)
+
+        except(Exception, psycopg2.DatabaseError) as error:
+            print("Deu caca\n")
+            print(error)
+            self.connection.rollback()
+            return 1
+
+        finally:
+            self.connection.commit()
+            return 0  
 
     async def executeImplicit(self):
         try:
@@ -36,17 +56,18 @@ class Banco:
                 ab = x + 10
                 query = "insert into product values( " + str(ab) + ", " + self.df['Product Name'] [x] + ")"
                 print(query)
-                #cursor.execute(
+                #cursor.execute(query)
         
         except (Exception, psycopg2.DatabaseError) as error:
             print("Deu caca\n")
             print(error)
             self.connection.rollback()
-        print('hello 1')
 
-    async def showRows(self):
-        pass
-
-bd = Banco()
-bd.connect()
-asyncio.run(bd.runTransaction())
+banco = Banco()
+banco.connect()
+# Banco.connect(self)
+asyncio.run(banco.runTransaction())
+# Banco.executeExplicit()
+# Banco.executeImplicit()
+# Banco.executeExplicitWithRollback()
+# Banco.executeImplicitWithRollback()
